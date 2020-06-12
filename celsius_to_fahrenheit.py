28a8677d55b8891e80e76bb3c6a5c27867404def
jonlebowitz <lebowitzjs@gmail.com>
2020-06-12 16:14:49 -0700

    Create celsius_to_fahrenheit.py

---
+++ celsius_to_fahrenheit.py
@@ -0,0 +1,4 @@
+Celsius=float(input("Input temperature in Celsius: "))                 
+Fahrenheit=(Celsius*1.8)+32                                           
+Fahrenheit_rounded=round(Fahrenheit,2)                                 
+print(Celsius,"Celsius equals",Fahrenheit_rounded,"Fahrenheit")
